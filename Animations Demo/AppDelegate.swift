b520eab248d3db76c14b18358b29f720192c526d
Brahmaiah Thota <tbn.iosdeveloper@gmail.com>
2017-03-15 13:26:03 +0530

    second commit

--- Animations Demo/AppDelegate.swift
+++ Animations Demo/AppDelegate.swift
@@ -12,7 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+//changes for testing 15/03/17
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
